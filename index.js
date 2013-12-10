66cf6c94b26123e17513cefb3cdbd22a7b9b0c53
Adam Stokes <adam.stokes@ubuntu.com>
2013-12-10 10:36:35 -0500

    start
    Signed-off-by: Adam Stokes <adam.stokes@ubuntu.com>

---
+++ index.js
@@ -0,0 +1,11 @@
+/* launchpad.net.js
+ * A nodejs client and authorization library for Launchpad.net
+ */
+'use strict';
+
+var path = require("path");
+var fs = require("fs");
+
+module.exports = function(scope, opts) {
+    // nothing here yet
+};
